5d200bce3631006b8d4320aee9e78dabe0f11bdf
Ayush <ayushmanojgupta24@gmail.com>
2020-05-13 21:37:13 +0530

    Calculating Wage of an Employee for 20 days anin EmpWageBuilder.java

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,6 +5,7 @@ public class EmpWageBuilder {
       public static final int IS_FULL_TIME = 2;
       public static final int EMP_RATE_PER_HOUR = 20;
 		public static final int NUM_OF_WORKING_DAYS = 20;
+		
 		public static void main(String[] args) {
 		//Variables
 		int empHrs = 0;
